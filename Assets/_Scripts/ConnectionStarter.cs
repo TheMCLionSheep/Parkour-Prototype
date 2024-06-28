27cbd09734a8884e98855597da34bf20e96932d1
Gabriel Iskandar <48334035+TheMCLionSheep@users.noreply.github.com>
2024-06-28 15:22:02 -0500

    Added parrel sync for faster dev time
    Added module for faster build + test time. Can create a clone of the unity project with Parrel Sync. Then can run the project on both ends, rather than needing to build a second version

---
+++ Assets/_Scripts/ConnectionStarter.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FishNet;
+using FishNet.Transporting;
+using FishNet.Transporting.Tugboat;
+using UnityEngine;
+
+public class ConnectionStarter : MonoBehaviour
+{
+    private Tugboat tugboat;
+
+    private void OnEnable()
+    {
+        InstanceFinder.ClientManager.OnClientConnectionState += OnClientConnectionState;
+    }
+
+    private void OnDisable()
+    {
+        InstanceFinder.ClientManager.OnClientConnectionState -= OnClientConnectionState;
+    }
+
+    private void OnClientConnectionState(ClientConnectionStateArgs args)
+    {
+        if (args.ConnectionState == LocalConnectionState.Stopping)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+
+    void Start()
+    {
+        if(TryGetComponent(out Tugboat t))
+        {
+            tugboat = t;
+        }
+        else
+        {
+            Debug.LogError("Couldn't get tugboat!", this);
+            return;
+        }
+
+        if (ParrelSync.ClonesManager.IsClone())
+        {
+            tugboat.StartConnection(false);
+        }   
+        else
+        {
+            tugboat.StartConnection(true);
+            tugboat.StartConnection(false);
+        }
+    }
+}
